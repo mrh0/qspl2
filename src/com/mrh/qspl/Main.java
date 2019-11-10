143b65866c9bd754d5d2196720eeb77440c1e424
MRH <mrhmail1@gmail.com>
2019-11-10 23:46:53 +0100

    v2.3.1
    + Flag '-output-code'
    + Copyright notice.

--- src/com/mrh/qspl/Main.java
+++ src/com/mrh/qspl/Main.java
@@ -20,6 +20,7 @@ import com.mrh.qspl.vm.VM;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Quick Statement Programming Language (QSPL) [ (C) MRH0 2019 ]");
 		if(args.length == 0) {
 			System.out.print("Run arguments: [path, (keys..)] but you gave: ");
 			System.out.print("[");
@@ -28,18 +29,20 @@ public class Main {
 			System.out.println("]. Run 'help'.");
 		}
 		
-		
+		boolean outputCode = false;
 		for(String k : args) {
 			if(k.equals("-d") || k.equals("-debug"))
 				Debug.enableDeepDebug = true;
 			if(k.equals("-nr") || k.equals("-no-result"))
 				Debug.noResult = true;
+			if(k.equals("-oc") || k.equals("-output-code"))
+				outputCode = true;
 		}
 		
 		String code = "";
 		if(args.length > 0) {
 			if(args[0].equals("help")) {
-				System.out.println("Run: [path, (keys..)]. Keys:(-debug, -no-result, -no-output)");
+				System.out.println("Run: [path, (keys..)]. Keys:(-debug, -no-result, -no-output, -output-code)");
 				return;
 			}
 			code = readFromFile(args[0]);
@@ -48,6 +51,8 @@ public class Main {
 			code = readFromFile("C:\\MRHLang\\debug.qs");
 		if(!Debug.noResult)
 			System.out.println("[PROGRAM OUTPUT]:");
+		if(outputCode)
+			System.out.println(code);
 		new QSPL().insertCode(code).execute();
 	}
 	
